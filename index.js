29e302a4e794d7cf4b886194025e9a03643c58a0
Laura Campbell <campbell.laura.elizabeth@gmail.com>
2018-02-08 00:58:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,27 @@ function theBeatlesPlay (musicians, instruments) {
   var index;
   for (index=0; index < musicians.length; index++) {
     str = musicians[index] + ' plays ' + instruments[index];
-    arr = arr + str;
+    arr[index] = str;
   }
+  return arr;
+}
+
+function johnLennonFacts (facts) {
+  var arr = [];
+  var str;
+  var index = 0;
+  while (index < facts.length) {
+    str = facts[index] + '!!!';
+    arr[index] = str
+    index++;
+  }
+  return arr;
+}
+
+function iLoveTheBeatles (num) {
+  var arr = [];
+  var index = 0;
+  do {arr[index] = 'I love the Beatles!'; index++; }
+  while (index <=num && num<15);
   return arr;
 }
